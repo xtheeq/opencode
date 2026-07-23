97bb2a82a468bad1160d1cde1afe95ba2146b1d2
xtheeq <atheeq.rhxn@gmail.com>
2026-07-23 14:14:27 +0530

    feat(mobile): add tanstack react query provider

--- packages/mobile/src/app/_layout.tsx
+++ packages/mobile/src/app/_layout.tsx
@@ -2,15 +2,18 @@ import { Slot } from "expo-router";
 import { ThemeProvider } from "@/theme";
 import { ConnectionProvider } from "@/services/connection";
 import { ErrorBoundary } from "@/components/error-boundary";
+import { QueryProvider } from "@/app/query-provider";
 
 export default function RootLayout() {
   return (
     <ErrorBoundary>
-      <ThemeProvider>
-        <ConnectionProvider>
-          <Slot />
-        </ConnectionProvider>
-      </ThemeProvider>
+      <QueryProvider>
+        <ThemeProvider>
+          <ConnectionProvider>
+            <Slot />
+          </ConnectionProvider>
+        </ThemeProvider>
+      </QueryProvider>
     </ErrorBoundary>
   );
 }
