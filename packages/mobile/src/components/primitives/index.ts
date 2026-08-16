2e7f55d84170dd1281c67e375d6f9854970ba95c
xtheeq <atheeq.rhxn@gmail.com>
2026-08-16 21:40:03 +0530

    chore(mobile): export dialog primitive

--- packages/mobile/src/components/primitives/index.ts
+++ packages/mobile/src/components/primitives/index.ts
@@ -2,3 +2,4 @@ export { Text, type TextProps } from "./text";
 export { TextInput } from "./text-input";
 export { Button, type ButtonProps } from "./button";
 export { BottomSheet, type BottomSheetProps } from "./bottom-sheet";
+export { Dialog, type DialogProps } from "./dialog";
